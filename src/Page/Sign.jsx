116609287ad77669b707a887a8f67848d1698afd
reflex2787 <jajoriyasahil2787@gmail.com>
2025-06-12 11:03:22 +0530

    Updated old code

--- src/Page/Sign.jsx
+++ src/Page/Sign.jsx
@@ -85,14 +85,14 @@ function Sign() {
               required
               className="mt-1 block w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
             />
-            <button
+            <div
             onClick={() => {setpassword(!password)}}
             className='absolute top-8 right-2'
             >
               {
                 password ? '🙈 Hide' : '👁️ Show'
               }
-            </button>
+            </div>
           </div>
           <button
             type="submit"
